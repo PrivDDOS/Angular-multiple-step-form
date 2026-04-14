7756e4b2ae5bb119d2fb883a41ee9185dc85e757
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-04-14 11:17:14 +0700

    creating form

---
+++ src/app/components/home/home-component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm} from '@angular/forms';
+import { UserInfo } from '../../models/class/user';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home-component',
+  standalone: true,
+  imports: [CommonModule ,FormsModule],
+  templateUrl: './home-component.html',
+  styleUrl: './home-component.css',
+})
+export class HomeComponent {
+  formStep = 1
+
+  userInfo = new UserInfo('', '', '')
+
+  onSubmit(form: NgForm) {
+
+    console.log('Your Data', this.userInfo)
+  }
+
+  nextStep() { 
+    this.formStep++; 
+  }
+
+}
